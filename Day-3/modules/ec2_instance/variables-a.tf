aef1fdfb7e9f558e6e4a66e2625533b6830e27c2
Amrendra Pratap Singh <130237717+amrendra0918@users.noreply.github.com>
2023-12-30 11:56:54 +0530

    Create variables-a.tf

---
+++ Day-3/modules/ec2_instance/variables-a.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+  description = "contains information about the ami id"
+  type = string
+}
+variable "instance_type_value" {
+  description = "contains value for instance type"
+  type = string
+}
+variable "subnet_id_value" {
+  description = "contains value for subnet id"
+  type = string
+}
